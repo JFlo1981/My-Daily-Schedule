527b175e3bcaa33b26a1b2d065570816f5ed1b94
JFlo1981 <jarod.florence@outlook.com>
2022-01-28 16:30:27 -0600

    sudo for JS added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
-var now = moment().format("dddd, MMMM Do, YYYY")
+const now = moment().format("dddd, MMMM Do, YYYY")
 $("#currentDay").append(now);
 
+// function to compare curent time to time-blocks for color change
+
+function pastPresentFuture() {
+//  add code for changing the color of the time-block based on past, presnet & future
+}
+
+
+// saved user text entry to local storage when save icon is clicked
+// key as event, event as value
+// creat event listener to put this key:value pair into local storage
+
+// when refreshed grab local storage data and display it in the appropriate time block
+
+// add call functions to run program
