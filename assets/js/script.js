7fb6b602705c7fbcd541a4554a793643f30a9ff1
JFlo1981 <jarod.florence@outlook.com>
2022-01-28 14:55:01 -0600

    added 'time-block' class to each row

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,3 @@
-// var date = new Date();
-// var now = date.toDateString();
-// var time = date.toLocaleTimeString();
-// document.getElementById("currentDay").innerHTML = now + ' - ' + time;
-
 var now = moment().format("dddd, MMMM Do, YYYY")
 $("#currentDay").append(now);
+
