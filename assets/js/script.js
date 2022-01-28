78362209e7760bb05309c7e344f77374993a6a37
JFlo1981 <jarod.florence@outlook.com>
2022-01-28 14:48:13 -0600

    updated JS with moment date format

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
-var date = new Date();
-var now = date.toDateString();
-var time = date.toLocaleTimeString();
-document.getElementById("currentDay").innerHTML = now + ' - ' + time;
+// var date = new Date();
+// var now = date.toDateString();
+// var time = date.toLocaleTimeString();
+// document.getElementById("currentDay").innerHTML = now + ' - ' + time;
+
+var now = moment().format("dddd, MMMM Do, YYYY")
+$("#currentDay").append(now);
