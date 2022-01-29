9eec9be8c81f361430e68042592861cd00e34857
JFlo1981 <jarod.florence@outlook.com>
2022-01-29 04:39:26 -0600

    function to compare currentTime to planerTime added

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,31 @@ $("#currentDay").append(now);
 // function to compare curent time to time-blocks for color change
 
 function pastPresentFuture() {
-//  add code for changing the color of the time-block based on past, presnet & future
-}
+//  need to add code for changing the color of the time-block based on past, presnet & future
+//  var for current time
+    const currentTime = moment().format("HH");
+    console.log(currentTime);
+
+// get id of element  -- found .attr()
+
+    $(".row").map(function(){
+      let  plannerTime = $(this).attr("id");
+      
+// compare with if stament and assign proper css (.addClass)
+      if (currentTime > plannerTime) {
+          $(this).addClass("past"); 
+      } else if (currentTime == plannerTime) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+
+    });
+//   console.log(plannerTime);
+// var for time-block
+
+
+};
 
 
 // saved user text entry to local storage when save icon is clicked
@@ -15,3 +38,4 @@ function pastPresentFuture() {
 // when refreshed grab local storage data and display it in the appropriate time block
 
 // add call functions to run program
+pastPresentFuture();
